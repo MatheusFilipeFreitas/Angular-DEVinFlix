d7984d88267a6554a2b648ff6be3d60b2c871c17
Math <mathffreitas@hotmail.com>
2023-03-05 10:07:26 -0300

    M1S9E04 - Criando componente menu
    Crie o componente de menu da aplicação Netflix.

--- netflix-replica/src/app/app.module.ts
+++ netflix-replica/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CardComponent } from './components/card/card.component';
 import { SerieComponent } from './pages/serie/serie.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CardComponent,
-    SerieComponent
+    SerieComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
