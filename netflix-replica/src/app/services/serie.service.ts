60fe9b95fbb446f939c2de961c79b31119f90f3a
Math <mathffreitas@hotmail.com>
2023-03-10 13:53:15 -0300

    M1S9E09 - Tratando erro de undefined

--- netflix-replica/src/app/services/serie.service.ts
+++ netflix-replica/src/app/services/serie.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Serie } from '../common/serie';
-import { Observable } from 'rxjs';
+import { Observable, catchError, retry } from 'rxjs';
 import { API_PATH } from 'src/environments/environments';
 
 @Injectable({
@@ -12,10 +12,14 @@ export class SerieService {
   constructor( private http: HttpClient ) { }
 
   getSeries(): Observable<Serie[]> {
-    return this.http.get<Serie[]>(`${API_PATH}/series`);
+    return this.http.get<Serie[]>(`${API_PATH}/series`).pipe(catchError(() => {
+      throw new Error("Failed to reach the server!");
+    }), retry({count: 2, delay: 500}));
   }
 
   getSerie(id: string): Observable<Serie> {
-    return this.http.get<Serie>(`${API_PATH}/series/${id}`);
+    return this.http.get<Serie>(`${API_PATH}/series/${id}`).pipe(catchError(() => {
+      throw new Error("Failed to reach the server!");
+    }), retry({count: 2, delay: 500}));;
   }
 }
